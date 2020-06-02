4264eea50d8e4a9ce38cde4424a9114d820a7489
nikolasmelui <slak@samaradom.ru>
2020-06-03 02:12:36 +0400

    Change the logic of proof.go method calls

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,10 +1,5 @@
 package blockchain
 
-import (
-	"bytes"
-	"crypto/sha256"
-)
-
 // BlockChain ...
 type BlockChain struct {
 	Blocks []*Block
@@ -15,19 +10,17 @@ type Block struct {
 	Hash     []byte
 	Data     []byte
 	PrevHash []byte
-}
-
-// DeriveHash ...
-func (b *Block) DeriveHash() {
-	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
-	hash := sha256.Sum256(info)
-	b.Hash = hash[:]
+	Nonce    int
 }
 
 // CreateBlock ...
 func CreateBlock(data string, prevHash []byte) *Block {
-	block := &Block{[]byte{}, []byte(data), prevHash}
-	block.DeriveHash()
+	block := &Block{[]byte{}, []byte(data), prevHash, 0}
+	pow := NewProof(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
 	return block
 }
 

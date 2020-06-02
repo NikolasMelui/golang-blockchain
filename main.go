eef503c3fc5cc8a8eb6e4db5e1fa0289167fa589
nikolasmelui <slak@samaradom.ru>
2020-06-03 01:05:09 +0400

    Create AddBlock method

--- main.go
+++ main.go
@@ -31,5 +31,12 @@ func CreateBlock(data string, prevHash []byte) *Block {
 	return block
 }
 
+// AddBlock ...
+func (chain *BlockChain) AddBlock(data string) {
+	prevBlock := chain.blocks[len(chain.blocks)-1]
+	new := CreateBlock(data, prevBlock.Hash)
+	chain.blocks = append(chain.blocks, new)
+}
+
 func main() {
 }

30bcb77c4d14b3dbae2a6ac4a876b3793f29afc2
nikolasmelui <slak@samaradom.ru>
2020-06-03 00:13:24 +0400

    Create the Block struct

--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import "log"
 
+// Block ...
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
+}
+
 func main() {
 	log.Println("First blockchain message")
 }

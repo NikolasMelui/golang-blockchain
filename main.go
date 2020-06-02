a82f889cc23daf58046f4e1d69e4883885c2cc94
nikolasmelui <slak@samaradom.ru>
2020-06-03 00:16:34 +0400

    Add DeriveHash function

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
-import "log"
+import (
+	"bytes"
+	"crypto/sha256"
+	"log"
+)
 
 // Block ...
 type Block struct {
 	Hash     []byte
 	Data     []byte
 	PrevHash []byte
+}
+
+// DeriveHash ...
+func (b *Block) DeriveHash() {
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+	hash := sha256.Sum256(info)
+	b.Hash = hash[:]
 }
 
 func main() {

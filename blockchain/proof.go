61ec1e6b046fd0edade95bc9bf688eb20f5ff4ef
nikolasmelui <slak@samaradom.ru>
2020-06-03 02:00:17 +0400

    Add the InitData and ToHex methods realization

--- blockchain/proof.go
+++ blockchain/proof.go
@@ -1,6 +1,11 @@
 package blockchain
 
-import "math/big"
+import (
+	"bytes"
+	"encoding/binary"
+	"log"
+	"math/big"
+)
 
 // Difficulty ...
 const Difficulty = 12
@@ -17,4 +22,27 @@ func NewProof(b *Block) *ProofOfWork {
 	target.Lsh(target, uint(256-Difficulty))
 	pow := &ProofOfWork{b, target}
 	return pow
+}
+
+// InitData ...
+func (pow *ProofOfWork) InitData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			pow.Block.PrevHash,
+			pow.Block.Data,
+			ToHex(int64(nonce)),
+			ToHex(int64(Difficulty)),
+		},
+		[]byte{})
+	return data
+}
+
+// ToHex ...
+func ToHex(num int64) []byte {
+	buff := new(bytes.Buffer)
+	err := binary.Write(buff, binary.BigEndian, num)
+	if err != nil {
+		log.Panic(err)
+	}
+	return buff.Bytes()
 }
